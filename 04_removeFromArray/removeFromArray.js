c892c4dd03af92fb5b6542d4c144a45094b28d8c
Ratilio <93900286+Ratilio@users.noreply.github.com>
2023-04-17 15:36:21 -0600

    Finished excercise 4. The first test was easy i think, but had to search for using multiple arguments in a function and then found the filter function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+  //the triple point in the arguments is to indicates multiple arguments but not in an array
+  
+  //filter removes from array the objects that "not" includes the object in the arguments
+  return array.filter(i => !remove.includes(i));
 };
 
 // Do not edit below this line

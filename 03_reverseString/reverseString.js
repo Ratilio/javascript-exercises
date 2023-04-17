d4611f720358e61e2be8bbfe6d1504263779f565
Ratilio <93900286+Ratilio@users.noreply.github.com>
2023-04-17 15:03:57 -0600

    Completed 3rd excercise. With the same function for all tests. Challenge was getting right the first test.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let word = ''; 
+  //sting.length-1 is necessary or it will leave a blank space "undefined"
+  for (let index = string.length-1; index >= 0; index--) {
+    word += string[index];
+  }
+  return word;
 };
 
 // Do not edit below this line

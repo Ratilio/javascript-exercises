6dca56deaf4cbd1a1ecbd39e0c59a9d253cfca53
Ratilio <93900286+Ratilio@users.noreply.github.com>
2023-04-17 14:26:47 -0600

    Completed excercise 2. Just the test 5 gave me a hard time because of me not using the correct variable in the conditional.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+  let word = '';
+  if (repetitions < 0) word = 'ERROR';
+  
+  for (let index = 0; index < repetitions; index++) {
+    word += string;
+  }
+  return word;
 };
 
 // Do not edit below this line

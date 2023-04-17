18b7664add48d9af3386ea152f27e7d7b739754e
Ratilio <93900286+Ratilio@users.noreply.github.com>
2023-04-17 16:06:59 -0600

    Completed excercise 5. The only problem was verifying if it was an integer or not, but used other function.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+  let sum = 0;
+  if (number1 < 0 || number2 < 0) return 'ERROR';
+  if (typeof number1 !== 'number' || typeof number2 !== 'number') return 'ERROR';
+  if (number1 < number2){
+    for (let i = number1; i <= number2; i++) {
+      sum += i;
+  
+    }
+  }
+  if (number1 > number2){
+    for (let i = number2; i <= number1; i++) {
+      sum += i;
+  
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line

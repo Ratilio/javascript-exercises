0cea342799c65f4555964be5a3d3308291d2d258
Ratilio <93900286+Ratilio@users.noreply.github.com>
2024-05-29 22:38:12 -0600

    Finished Object Basics section of course. hehe did not commit regularly. Took some days.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, c) {
+  return a + c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, index) => total + index, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiplier, product) => multiplier * product, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);	
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0) return 1;
+  let total = 1;
+  for (let i = n; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (array) {
+  let punctuation = /[^a-z]/g;//regex to only accept letters
+  let newArray = array.toLowerCase().replace(punctuation, "");//creates new array, transform to lower case and eliminates punctuation and glues all the letters into one element
+  let reverseArray = newArray.split('').reverse().join('');//creates new array, splits every letter into elements of array, reverse it and joins the array into one element
+  return reverseArray === newArray;
 };
 
 // Do not edit below this line
